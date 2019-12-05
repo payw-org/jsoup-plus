d89ffcdd78843b883a4433f221a4336c126508f2
ihooni <to@ihooni.com>
2019-12-06 02:55:27 +0900

    chore: change SQLish API by updated SQLCommand

--- src/main/java/org/jsoup/helper/SQLish.java
+++ src/main/java/org/jsoup/helper/SQLish.java
@@ -7,30 +7,43 @@ import java.util.ArrayList;
 public class SQLish {
     private Elements elements;
 
+    private TextExtractor extractor;
+
     private ArrayList<SQLCommand> commands;
 
     public SQLish(Elements elements) {
         this.elements = elements;
+        this.extractor = new TextExtractor.WholeText();
         this.commands = new ArrayList<>();
     }
 
+    public SQLish(Elements elements, TextExtractor extractor) {
+        this.elements = elements;
+        this.extractor = extractor;
+        this.commands = new ArrayList<>();
+    }
+
+    public void setExtractor(TextExtractor extractor) {
+        this.extractor = extractor;
+    }
+
     public SQLish orderByTextAsc() {
-        this.commands.add(new SQLCommand.OrderByTextAscCommand());
+        this.commands.add(new SQLCommand.OrderByTextAscCommand(this.extractor));
         return this;
     }
 
     public SQLish orderByTextDesc() {
-        this.commands.add(new SQLCommand.OrderByTextDescCommand());
+        this.commands.add(new SQLCommand.OrderByTextDescCommand(this.extractor));
         return this;
     }
 
     public SQLish startsWithText(String prefix) {
-        this.commands.add(new SQLCommand.StartsWithText(prefix));
+        this.commands.add(new SQLCommand.StartsWithText(this.extractor, prefix));
         return this;
     }
 
     public SQLish endsWithText(String suffix) {
-        this.commands.add(new SQLCommand.EndsWithText(suffix));
+        this.commands.add(new SQLCommand.EndsWithText(this.extractor, suffix));
         return this;
     }
 

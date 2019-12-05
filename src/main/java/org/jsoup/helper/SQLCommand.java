3403f286d4707e796892f2977846acc5921773ff
ihooni <to@ihooni.com>
2019-12-06 01:42:47 +0900

    chore: clean code

--- src/main/java/org/jsoup/helper/SQLCommand.java
+++ src/main/java/org/jsoup/helper/SQLCommand.java
@@ -3,10 +3,7 @@ package org.jsoup.helper;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.*;
 
 public abstract class SQLCommand {
     public abstract void execute(Elements elements);
